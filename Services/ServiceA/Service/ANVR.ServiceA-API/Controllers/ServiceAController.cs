7a4f5b3154da7225e74fdf0966e9b2a3349a960b
Anish <anishmarokey@gmail.com>
2023-07-18 20:56:40 +0530

    added http endpoint to test

--- Services/ServiceA/Service/ANVR.ServiceA-API/Controllers/ServiceAController.cs
+++ Services/ServiceA/Service/ANVR.ServiceA-API/Controllers/ServiceAController.cs
@@ -6,9 +6,39 @@ namespace ANVR.ServiceA_API.Controllers;
 [Route("api/[controller]")]
 public class ServiceAController : ControllerBase
 {
+    #region Private Variables
+    private readonly IHttpClientFactory _httpClientFactory;
+    #endregion
 
+    #region Constructor
+    public ServiceAController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+    #endregion
+ 
+    #region Endpoints
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
     [HttpGet]
     [Route("IsHealthy")]
     public string IsHealthy()=> "Service A is healthy";
+
+    [HttpGet]
+    [Route("GetApiData")]
+    public async Task<IActionResult> GetApiData([FromQuery] string apiUrl)
+    {           
+        HttpClient httpClient = _httpClientFactory.CreateClient();
+        HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+        // Ensure the response is successful
+        response.EnsureSuccessStatusCode();
+        // Read the response content as a string
+        string content = await response.Content.ReadAsStringAsync();
+            return Ok(content);
+        
+    }
+    #endregion
 }
